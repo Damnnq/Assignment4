f3aaeeed4bc1035bb25a15f74e6658469a379dc5
Bekdos <241396@astanait.edu.kz>
2025-05-17 17:57:15 +0500

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        WeightedGraph<String> graph = new WeightedGraph<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        fillWithWeights(graph);
+
+        System.out.println("Dijkstra:");
+        Search<String> dijkstra = new DijkstraSearch<>(graph, "Winterfell");
+        outputPath(dijkstra, "Riverrun");
+
+        System.out.println("BFS:");
+        Search<String> bfs = new BreadthFirstSearch<>(graph, "Winterfell");
+        outputPath(bfs, "Riverrun");
+    }
+
+    public static void fillWithWeights(WeightedGraph<String> graph) {
+        graph.addEdge("Winterfell", "King’s Landing", 2.1);
+        graph.addEdge("Dragonstone", "Harrenhal", 7.8);
+        graph.addEdge("Harrenhal", "King’s Landing", 7.1);
+        graph.addEdge("Winterfell", "Dragonstone", 7.2);
+        graph.addEdge("Dragonstone", "King’s Landing", 3.9);
+        graph.addEdge("King’s Landing", "Kostanay", 3.5);
+        graph.addEdge("Dragonstone", "Riverrun", 5.4);
+    }
+// игра престолов))
+    public static void outputPath(Search<String> search, String key) {
+        if (!search.hasPathTo(key)) {
+            System.out.println("No path to " + key);
+            return;
         }
+
+        for (String v : search.pathTo(key)) {
+            System.out.print(v + " -> ");
+        }
+        System.out.println("END");
     }
 }
